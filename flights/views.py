2271ec1346643828f540f9b85d64ba40a6e98315
mmrg98 <mariam.elgaria98@gmail.com>
2020-09-15 10:23:54 +0300

    Done

--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,26 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from datetime import datetime
 
 from .models import Flight, Booking
 from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer
 
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.name
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 class FlightsList(ListAPIView):
 	queryset = Flight.objects.all()
@@ -21,6 +38,15 @@ class BookingDetails(RetrieveAPIView):
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
 
+class CreateBooking(CreateAPIView):
+	serializer_class = UpdateBookingSerializer
+
+	def perform_create(self, serializer):
+		flight_id= self.kwargs['flight_id']
+		serializer.save(
+		user=self.request.user,
+		flight = Flight.objects.get(id=flight_id)
+		)
 
 class UpdateBooking(RetrieveUpdateAPIView):
 	queryset = Booking.objects.all()
--- task_1/urls.py
+++ task_1/urls.py
@@ -16,13 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from flights import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
-    
-    path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
+
+    path('bookings/', views.BookingsList.as_view(), name="bookings-list"),
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
+    path('booking/create/<int:flight_id>', views.CreateBooking.as_view(), name="book-flight"),
     path('booking/<int:booking_id>/update/', views.UpdateBooking.as_view(), name="update-booking"),
     path('booking/<int:booking_id>/cancel/', views.CancelBooking.as_view(), name="cancel-booking"),
+    path('login', TokenObtainPairView.as_view(), name='login'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
